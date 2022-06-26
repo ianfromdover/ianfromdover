171a6024331dc0129a567af6e0112d6362d74727
ian-from-dover <ian09hong@gmail.com>
2022-06-26 23:10:34 +0800

    Remove update filed

--- updateAliases.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Updates the Aliases file
-rm aliases.txt
-cp ~/.aliases aliases.txt
--- updateVimrc.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Updates the Aliases file
-rm vimrc.txt
-cp ~/.vimrc vimrc.txt

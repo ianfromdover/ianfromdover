88719df7e5c90011163cdb2300e0d2527b374621
Ian <ianfromdover@gmail.com>
2025-01-31 17:40:10 +0800

    fix(site): Check deployment

--- website/src/App.js
+++ website/src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Hurray! it works!</p>
+        <p>With Sass!</p>
         <a
           className="App-link"
           href="https://reactjs.org"

6a6c45a30aa7e5851800b5e9fd27306a48cc15dd
Ian <ianfromdover@gmail.com>
2025-03-31 01:21:36 +0800

    fix(site): Add output options

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  // distDir: 'build', // usually it is .next
+  // output: 'export', // gives static web app
 }
  
 const withMDX = createMDX({
